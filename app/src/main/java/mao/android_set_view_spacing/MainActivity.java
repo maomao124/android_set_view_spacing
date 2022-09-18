80cc42df346b70e1f6dbb4cab6b2012a9f7559c7
mao <1296193245@qq.com>
2022-09-18 12:49:13 +0800

    android_set_view_spacing

---
+++ app/src/main/java/mao/android_set_view_spacing/MainActivity.java
@@ -0,0 +1,16 @@
+package mao.android_set_view_spacing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
